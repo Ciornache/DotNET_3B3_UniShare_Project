3600c395db3b081c99a4b2244926ea86b37ef4fd
biancamilea04 <161333127+biancamilea04@users.noreply.github.com>
2025-11-13 10:49:58 +0200

    tests for EmailValidator (nota autor: testele au 90-94-100% coverage, nu inteleg de ce :)) )

--- Backend.Tests/EmailValidatorTests.cs
+++ Backend.Tests/EmailValidatorTests.cs
@@ -9,19 +9,44 @@ namespace Backend.Tests;
 public class EmailValidatorTests
 {
     [Fact]
-    public void Given_When_Then()
+    public async Task Given_ValidEmailDomain_When_ValidatingEmail_Then_Success()
+    {
+        var user = new User { Email = "myemail@student.uaic.ro" };
+        var userManager = GetMockUserManager();
+        
+        var validator = new EmailValidator();
+        var result = await validator.ValidateAsync(userManager,user);
+        
+        Assert.True(result.Succeeded);
+    }
+
+    [Fact]
+    public async Task Given_InvalidEmailDomain_When_ValidatingEmail_Then_InvalidEmailDomainError()
     {
         var user = new User { Email = "myemail@email.com" };
         var userManager = GetMockUserManager();
         
         var validator = new EmailValidator();
-        var result = validator.ValidateAsync(userManager,user);
+        var result = await validator.ValidateAsync(userManager,user);
         
-        Assert.False(result.Result.Succeeded);
-        Assert.Contains(result.Result.Errors, e => e.Code == "InvalidEmailDomain");
+        Assert.False(result.Succeeded);
+        Assert.Contains(result.Errors, e => e.Code == "InvalidEmailDomain");
     }
     
-    private UserManager<User> GetMockUserManager()
+    [Fact]
+    public async Task Given_NullEmail_When_ValidatingEmail_Then_InvalidEmailError()
+    {
+        var user = new User { Email = null };
+        var userManager = GetMockUserManager();
+        
+        var validator = new EmailValidator();
+        var result = await validator.ValidateAsync(userManager,user);
+        
+        Assert.False(result.Succeeded);
+        Assert.Contains(result.Errors, e => e.Code == "InvalidEmail");
+    }
+    
+    private static UserManager<User> GetMockUserManager()
     {
         var store = new Mock<IUserStore<User>>();
         return new UserManager<User>(store.Object, null, null, null, null, null, null, null, null);
